bbf6a4624dd7025ec38311823d5af621ad19ea4a
LionArtCodingStudio <leonardoweb25@gmail.com>
2026-07-02 02:32:18 -0300

    modificao para o script pegar os dados do json interno ao inves de tentar requisitar do github

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-async function getGithubInfo(owner, repo){
-    const url = 'https://github.com{owner}/${repo}';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-};
+async function getGithubInfo(id) {
+  try {
+    const response = await fetch('projetos.json');
+    
+    const projetos = await response.json();
+    
+    const data = projetos.find(projeto => projeto.id === Number(id));
+    
+    if (data) {
+      console.log(data);
+      console.log(`Projeto: ${data.nome}`); 
+    } else {
+      console.log(`Projeto com o ID ${id} não foi encontrado.`);
+    }
 
-getGithubInfo('LionArtCodingStudio', 'LionArtCodingStudio');
+  } catch (error) {
+    console.error("Erro ao buscar os dados:", error);
+  }
+}
 
-template (){
+// Executa a função
+getGithubInfo('1');
+getGithubInfo('2');
 
-};
