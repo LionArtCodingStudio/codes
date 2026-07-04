8695b58ae17526b0c55f6b2046850d2cebfb8422
LionArtCodingStudio <leonardoweb25@gmail.com>
2026-07-04 00:30:49 -0300

    prepsraçao para automatizar os projetos

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ async function getProjectInfo(id) {
 // Executa a função
 has_project = true; //manten o loop aberto enquanto houver projetos
 do {
-  for (let i = 1; i <= 10; i++) { // Supondo que você tenha 10 projetos
+  for (let i = 1; i <= 10; i++) { // Supondo que tenha 10 projetos
     const data = await getProjectInfo(i);
     if (data) {
       console.log(`Projeto: ${data.id} - ${data.nome}`);
       console.log(`Data de Início: ${data.data_publicacao}`);
       console.log(`Descrição: ${data.descricao}`);
+      template
     }
     if (!data) {
       has_project = false;
