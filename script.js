8a8bd9e0bc3734c3ef0090b5e97a1c7c2d997ba9
LionArtCodingStudio <leonardoweb25@gmail.com>
2026-07-04 14:22:25 -0300

    script alteration

--- script.js
+++ script.js
@@ -31,5 +31,22 @@ do {
 } while (has_project==true); 
 
 async function getPageInfo(id) {
-  const projetosContainer = document.querySelector ('');
+
+  const projetosContainer = document.querySelector ('.projetos');
+  const template = document.getElementById('.template');
+
+  fecth ('Projetos.json')
+  .then(response => response.json())
+  .then(data => {
+    data.foreach(projeto => {
+      const clone = template.content.cloneNode(true);
+      clone.querySelector('.nome').textContent = projeto.nome;
+      clone.querySelector('.data_publicacao').textContent = projeto.data_publicacao;
+      clone.querySelector('.descricao').textContent = projeto.descricao;
+      
+      const link = clone.querySelector('a');
+      link.href = `projeto.html?id=${projeto.id}`;
+    });
+  })
+  .catch(error => console.error('Erro ao carregar os projetos:', error));
 };
